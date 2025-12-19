4887ab509d1482ac6e2189f72ccf81e0e9ef4d1a
lucavalentebit <lucavalentebit@gmail.com>
2025-12-19 12:04:55 +0100

    Added basic logic just as a test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <raylib.h>
 using namespace std;
 
-int main() {
+#define width 30;
+#define height 30;
+
+class cell{
+    public:
+    int x;
+    int y;
+    bool state = false;
+}
+
+int main(void) {
+
+    InitWindow(width, height, "Test Window");
+    while (!WindowIsClosed){
+        BeginDrawing();
+    }
     return 0;
 }
